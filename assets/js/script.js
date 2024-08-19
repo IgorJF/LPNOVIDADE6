a9d1357b33ff74f61aada183d0bbc4027ba3088a
Igor Ferraz <igor.jferraz@yahoo.com>
2024-08-19 15:17:27 -0300

    att

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
-// Script de contagem regressiva
 const minutes = 10;
 let time = minutes * 60; // 10 minutos em segundos
+let vagas = 10; // Número inicial de vagas
 
 const countdownElement = document.querySelector('.countdown');
+const vagasElement = document.querySelector('.vagas');
 
 function updateCountdown() {
     const minutes = Math.floor(time / 60);
@@ -18,4 +19,13 @@ function updateCountdown() {
     }
 }
 
-const countdownInterval = setInterval(updateCountdown, 1000);
+function updateVagas() {
+    if (vagas > 0) {
+        vagas--;
+        vagasElement.innerText = `${vagas} VAGAS`;
+    }
+}
+
+const countdownInterval = setInterval(updateCountdown, 1000); // Atualiza a contagem regressiva a cada segundo
+
+setInterval(updateVagas, 5000); // Atualiza as vagas a cada 10 segundos
